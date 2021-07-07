52895b9869931060054768cdbe4307986975eb1d
rosewhat <whatcodingrose@gmail.com>
2021-07-07 13:54:49 +0300

    Initial Commit

---
+++ GCD/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GCD
+//
+//  Created by rose on 07.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
